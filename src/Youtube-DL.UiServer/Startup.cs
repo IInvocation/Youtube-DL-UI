bb60c7c37a02a6e723ff5604ba13fe280d7966b9
Achim Schnell <a.schnell@wtschnell.de>
2019-10-25 15:09:40 +0200

    Added mvc

--- src/Youtube-DL.UiServer/Startup.cs
+++ src/Youtube-DL.UiServer/Startup.cs
@@ -1,35 +1,84 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Youtube_DL.UiServer.Localization;
 
 namespace Youtube_DL.UiServer
 {
+    /// <summary>	A startup. </summary>
+    /// <remarks>
+    ///     1: Startup - used to initialize configuration
+    ///     2: ConfigureServices - loads all required services
+    ///     3: Configure - actual pipeline using the services
+    /// </remarks>
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        #region Properties
+
+        /// <summary>	Gets the configuration. </summary>
+        /// <value>	The configuration. </value>
+        public IConfigurationRoot Configuration { get; }
+
+        /// <summary>Gets the environment.</summary>
+        /// <value>The environment.</value>
+        public IWebHostEnvironment Environment { get; }
+
+        #endregion
+
+        /// <summary>Constructor.</summary>
+        /// <param name="environment">  . </param>
+        /// <remarks>Adds configuration from json-Files and environment-variables.</remarks>
+        public Startup(IWebHostEnvironment environment)
         {
+            Environment = environment;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", true);
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // MVC
+            services
+                .AddMvc(mvcOptions => { 
+                    mvcOptions.EnableEndpointRouting = false; 
+                })
+                .AddViewLocalization()
+                .AddDataAnnotationsLocalization()
+                .AddRazorOptions(razorOptions => {
+                    razorOptions.ViewLocationExpanders.Add(new CultureSubFolderViewLocationExpander());
+                });
+
+            // Proxy-Support
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders =
+                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseForwardedHeaders();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                // ui-controllers
+                routes.MapRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                // api-controllers
+                routes.MapRoute(
+                    "api",
+                    "api/[controller]");
             });
         }
     }
